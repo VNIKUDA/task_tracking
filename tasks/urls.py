08f823ea6a65b56bbe520c6ce8c5d7aa4fbfea84
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-07-06 13:24:11 +0300

    Update and Delete views'

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.TaskListView.as_view(), name="task-list"),
     path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
     path("task-create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("task-update/<int:pk>/", views.TaskUpdateView.as_view(), name="task-update"),
+    path("task-delete/<int:pk>/", views.TaskDeleteView.as_view(), name="task-delete")
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from tasks.forms import TaskForm
 from tasks import models
 
@@ -9,6 +9,7 @@ class TaskListView(ListView):
     model = models.Task
     context_object_name = "tasks"
     template_name = "tasks/task_list.html"
+    paginate_by = 7
 
 
 class TaskDetailView(DetailView):
@@ -16,9 +17,24 @@ class TaskDetailView(DetailView):
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
 
+
 class TaskCreateView(CreateView):
     model = models.Task
     template_name = "tasks/task_form.html"
     form_class = TaskForm
     success_url = reverse_lazy("tasks:task-list")
 
+
+class TaskUpdateView(UpdateView):
+    model = models.Task
+    template_name = "tasks/task_form.html"
+    form_class = TaskForm
+
+    def get_success_url(self) -> str:
+        pk = self.kwargs["pk"]
+        return reverse_lazy("tasks:task-detail", kwargs={"pk": pk})
+
+class TaskDeleteView(DeleteView):
+    model = models.Task
+    template_name = "tasks/task_delete.html"
+    success_url = reverse_lazy("tasks:task-list")
